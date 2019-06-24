0c8866e286aa1b6517fddfad92b339132bb306b0
kabelo <kabelo@thutolwethu.com>
2019-06-25 00:09:49 +0200

    first commit

---
+++ app/app.ts
@@ -0,0 +1,39 @@
+import express from 'express'; 
+import graphqlHTTP from 'express-graphql';
+import { makeExecutableSchema } from 'graphql-tools';
+
+const app: express.Application = express(); 
+const port = 3000;
+
+let typeDefs: any = [`
+    type Query {
+        hello: String
+    }
+
+    type Mutation {
+        hello(message: String) : String
+    }
+`];
+
+let helloMessage: String = 'World!';
+
+let resolvers = {
+    Query: {
+        hello: () => helloMessage
+    }, 
+    Mutation: {
+        hello: (_: any, helloData: any) => {
+            helloMessage = helloData.message;
+            return helloMessage;
+        }
+    }
+};
+
+app.use(
+    '/graphql', 
+    graphqlHTTP({
+        schema: makeExecutableSchema({typeDefs, resolvers}),
+        graphiql: true
+    })
+);
+app.listen(port, () => console.log(`Node Graphql API listening on port ${port}!`));
---
+++ app/users/users.service.ts
@@ -0,0 +1,49 @@
+const cryp = require('crypto');
+
+export class UsersService {
+
+    public users: any = [];
+    
+    configTypeDefs() {
+        let typeDefs = `
+        type User {
+            firstName: String, 
+            lastName: String, 
+            id: Int, 
+            password: String, 
+            permissionLevel: Int,
+            email: String
+        }`;
+        typeDefs += `
+        extend type Query {
+            users: [User]
+        }`;
+        typeDefs += `
+        extend type Mutation{
+            user(firstName: String,
+                lastName: String, 
+                password: String, 
+                permisssionLevel: Int,
+                email: String, 
+                id: Int): User!
+        }`;
+
+        return typeDefs;
+
+    }
+
+    configResolvers(resolvers: any) {
+        resolvers.Query.users = () => {
+            return this.users;
+        }; 
+        resolvers.Mutation.user = (_: any, user: any) => {
+            let salt = cryp.randomBytes(16).toString('base64');
+            let hash = cryp.createHmac('sha512', 
+                salt).update(user.password).digest("base64");
+            user.password = hash; 
+            this.users.push(user);
+            return user;
+        };
+    }
+
+}
